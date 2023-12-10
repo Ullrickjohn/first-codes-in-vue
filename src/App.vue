6ac1cf8f2dff4a9b607715b17ab6cecf4f645cb2
John <ullrickjojn@gmail.com>
2023-12-10 21:56:59 +0500

    written first codes in vue js

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template >
+<div>
+    <ul class="text">
+        <li class="text-item">
+            Fucking Hello
+
+        </li>
+    </ul>
+</div>
+</template>
+<script>
+export default{
+   
+}
+</script>
+<style>
+.text{
+    text-transform: uppercase;
+    text-align: center;
+    font-family: Arial, Helvetica, sans-serif;
+    list-style: none;
+}
+</style>
